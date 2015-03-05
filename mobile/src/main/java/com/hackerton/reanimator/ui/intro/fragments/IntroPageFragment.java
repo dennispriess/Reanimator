b8e5622a374fee20e94afdd692f328880d459b98
Matthias Bruns <info@matthiasbruns.com>
2015-03-05 20:19:54 +0100

    done

--- mobile/src/main/java/com/hackerton/reanimator/ui/intro/fragments/IntroPageFragment.java
+++ mobile/src/main/java/com/hackerton/reanimator/ui/intro/fragments/IntroPageFragment.java
@@ -5,9 +5,11 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.hackerton.reanimator.R;
 import com.hackerton.reanimator.ui.BaseFragment;
+import com.hackerton.reanimator.ui.reanimation.activities.ReanimationActivity;
 
 /**
  * Created by traxdata on 05/03/15.
@@ -49,7 +51,16 @@ public class IntroPageFragment extends BaseFragment {
         }
 
         final View view = inflater.inflate(resource, container, false);
-
+        Button startButton = (Button) view.findViewById(R.id.start);
+        if (startButton != null) {
+            startButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(ReanimationActivity.getIntent(getActivity()));
+                    getActivity().finish();
+                }
+            });
+        }
         return view;
     }
 
